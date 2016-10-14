6c4953115f3292ee0b4a9fcea0ae57c55a55447c
cowgoat88 <kgoetz32@gmail.com>
2016-10-14 02:48:42 -0700

    return functional data

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ vid_dir_path = sys.argv[1]
 
 
 def vid_parse(vid_dir_path):
-	vid_tags = []
+
 	for vid in os.listdir(vid_dir_path):
 		if not vid.startswith('.') and not vid.startswith('$'):
-			cmd = ['python', main_path+'py/apps/videofacerec/videofacerec.py',
-			main_path+'py/apps/videofacerec/model.pkl',
-			'-f',
-			os.path.join(vid_dir_path, vid)]
-			proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-			line = proc.stdout.readline()
-			json_line = line.replace("'", "\"")
+			cmd = ['python',
+				main_path+'py/apps/videofacerec/videofacerec.py',
+				main_path+'py/apps/videofacerec/model.pkl',
+				'-f',
+				os.path.join(vid_dir_path, vid)]
+			proc = subprocess.check_output(cmd)
 
-			return json.loads(json_line)
+			return proc
+			#return json.loads(json_line)
 
 
 print vid_parse(vid_dir_path)

cf2734191e51fcb2c69128db50b478d8cab60ffd
Stephen Hipskind <Stephen@Stephen.hsd1.ca.comcast.net>
2016-10-14 01:24:54 -0700

    return json

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 import os
 import subprocess
+import json
 
 main_path = '/Users/Stephen/Documents/Code/AceFace/'
 os.chdir(main_path+'py/apps/videofacerec/')
 vid_dir_path = sys.argv[1]
+
 
 def vid_parse(vid_dir_path):
 	vid_tags = []
 	for vid in os.listdir(vid_dir_path):
 		if not vid.startswith('.') and not vid.startswith('$'):
 			cmd = ['python', main_path+'py/apps/videofacerec/videofacerec.py', main_path+'py/apps/videofacerec/model.pkl', '-f', os.path.join(vid_dir_path, vid)]
-			proc = subprocess.check_output(cmd)
-			vid_tags.append(proc)
-	return vid_tags
+			proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+			line = proc.stdout.readline()
+			json_line = line.replace("'", "\"")
+			return json.loads(json_line)
 
 print vid_parse(vid_dir_path)
